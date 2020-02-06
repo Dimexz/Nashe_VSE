dfe7f12d106b637ca7d21ba1e23cc32ddf2cee84
Dimexz <dmnoykin@gmail.com>
2020-02-06 17:59:08 +0700

    Chto-to ochen` vasnoe

---
+++ test1-master/Assets/Script/Main.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    public Canvas canvas;
+    public GameObject Second_menu;
+    public GameObject Fird_menu;
+  
+
+    public void Second_menus()
+    {
+        Second_menu.SetActive(!Second_menu.activeSelf);
+    }
+
+    public void Fird_menus()
+    {
+        Fird_menu.SetActive(!Fird_menu.activeSelf);
+        Second_menu.SetActive(!Second_menu.activeSelf);
+    }
+
+    public void Scens()
+    {
+        SceneManager.LoadScene("XIX");
+    }
+
+    public void Scens2()
+    {
+        SceneManager.LoadScene("XX");
+    }
+
+
+    void Update()
+    {
+       
+    }
+}
